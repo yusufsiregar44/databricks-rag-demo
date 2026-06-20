1625129767d53799ba7a38c77b87f1084cd6553f
Yusuf Siregar <33023147+yusufsiregar44@users.noreply.github.com>
2026-06-20 07:09:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,71 @@
+import itertools
 import gradio as gr
+import requests
+import os
+from gradio.themes.utils import sizes
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", "slider"],
-    outputs=["text"],
+def respond(message, history):
+
+    if len(message.strip()) == 0:
+        return "ERROR the question should not be empty"
+
+    local_token = os.getenv('API_TOKEN')
+    local_endpoint = os.getenv('API_ENDPOINT')
+
+    if local_token is None or local_endpoint is None:
+        return "ERROR missing env variables"
+
+    # Add your API token to the headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {local_token}'
+    }
+
+    #prompt = list(itertools.chain.from_iterable(history))
+    #prompt.append(message)
+    #q = {"inputs": [prompt]}
+    q = {"inputs": [message]}
+    try:
+        response = requests.post(
+            local_endpoint, json=q, headers=headers, timeout=100)
+        response_data = response.json()
+        #print(response_data)
+        response_data=response_data["predictions"][0]
+        #print(response_data)
+
+    except Exception as error:
+        response_data = f"ERROR status_code: {type(error).__name__}"
+        # + str(response.status_code) + " response:" + response.text
+
+    # print(response.json())
+    return response_data
+
+
+theme = gr.themes.Soft(
+    text_size=sizes.text_sm,radius_size=sizes.radius_sm, spacing_size=sizes.spacing_sm,
 )
 
-demo.launch()
+
+demo = gr.ChatInterface(
+    respond,
+    chatbot=gr.Chatbot(show_label=False, container=False, show_copy_button=True, bubble_full_width=True),
+    textbox=gr.Textbox(placeholder="Ask me a question",
+                       container=False, scale=7),
+    title="Databricks LLM RAG demo - Chat with DBRX Databricks model serving endpoint",
+    description="This chatbot is a demo example for the dbdemos llm chatbot.<br> It is using DBRX, with an extra touch of Homer Simpson's quote at every answer.<br> Made with ❤️ by Yusuf Siregar.",
+    examples=[["What is DBRX?"],
+              ["How can I start a Databricks cluster?"],
+              ["What is a Databricks Cluster Policy?"],
+              ["How can I track billing usage on my workspaces?"],],
+    cache_examples=False,
+    theme=theme,
+    retry_btn=None,
+    undo_btn=None,
+    clear_btn="Clear",
+)
+
+if __name__ == "__main__":
+    demo.launch()
+
+demo.launch(share=True)
